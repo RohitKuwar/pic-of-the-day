132bf2b7e7642409f531bed44d5b95d917c59f3c
RohitKuwar <70262783+RohitKuwar@users.noreply.github.com>
2021-03-21 18:22:48 +0530

    pic of the day

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,24 +1,104 @@
-import React from 'react';
-import styles from './pagination.module.css';
-
-const Pagination = ({ dataPerPage, totalData, paginate }) => {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
+import React, { useEffect, useState } from "react";
+import styles from "./pagination.module.css";
+const renderData = (data) => {
   return (
-      <div>
-        {pageNumbers.map(number => (
-          <span className={styles.numbers} key={number}>
-            <a onClick={() => paginate(number)} href='!#'>
-              {number}
-            </a>
-          </span>
-        ))}
-      </div>
+    <div className={styles.container}>
+      {
+        data.map((item, index) => (
+          <div className={styles.box} key={index}>
+            <div className={styles.imageBox}>{item.userId}</div>
+            <div className={styles.info}>
+              <h3>{item.title}</h3>
+              <h4>{item.id}</h4>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   );
 };
+
+function Pagination() {
+  const [data, setData] = useState([]);
+  const [currentPage, setcurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [pageNumberLimit] = useState(10);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(10);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
+
+  const handleClick = (event) => {
+    setcurrentPage(Number(event.target.id));
+  };
+
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pages.push(i);
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const renderPageNumbers = pages.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={handleClick}
+          className={`currentPage === number ? ${styles.active} : null`}>
+          {number}
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  const handleNextbtn = () => {
+    setcurrentPage(currentPage + 1);
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const handlePrevbtn = () => {
+    setcurrentPage(currentPage - 1);
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  return (
+    <>
+      {renderData(currentItems)}
+      <ul className={styles.pageNumbers}>
+        <li>
+          <button
+            onClick={handlePrevbtn}
+            disabled={currentPage === pages[0] ? true : false}>
+            Prev
+          </button>
+        </li>
+        {renderPageNumbers}
+        <li>
+          <button
+            onClick={handleNextbtn}
+            disabled={currentPage === pages[pages.length - 1] ? true : false}>
+            Next
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+}
 
 export default Pagination;
--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,34 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Heading from '../components/heading/Heading';
-import Data from '../components/data/Data';
 import Pagination from '../components/pagination/Pagination';
 
 function SearchResult({text}) {
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [dataPerPage] = useState(10);
-
-  useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res=>res.json())
-      .then(res=>setData(res))
-  }, []);
-
-  const indexOfLastData = currentPage * dataPerPage;
-  const indexOfFirstData = indexOfLastData - dataPerPage;
-  const currentData = data.slice(indexOfFirstData, indexOfLastData);
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
       <Heading />
       <h3 style={{marginLeft: '.5em'}}>search result for {text}</h3>
-      <Data data={currentData} />
-      <Pagination
-        dataPerPage={dataPerPage}
-        totalData={data.length}
-        paginate={paginate}
-      />
+      <Pagination />
     </div>
   )
 }
